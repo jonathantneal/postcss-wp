ca5ab398be2d4dc49b2ce2f94ab5858e461f8492
Jonathan Neal <jonathantneal@hotmail.com>
2015-06-25 00:53:24 -0400

    Update tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var postcss = require('postcss');
+var expect  = require('chai').expect;
+
+var plugin = require('../');
+
+var test = function (input, output, opts, done) {
+	postcss([ plugin(opts) ]).process(input).then(function (result) {
+		expect(result.css).to.eql(output);
+		expect(result.warnings()).to.be.empty;
+		done();
+	}).catch(function (error) {
+		done(error);
+	});
+};
+
+describe('postcss-wp', function () {
+	it('uses css', function (done) {
+		test('html{body{}}', 'html{body{}}', {}, done);
+	});
+});
